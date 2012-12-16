658164fa50dce47526b89c88423a0d4a609a20c8
Sam Tran <samvtran@gmail.com>
2012-12-16 06:53:49 -0600

    Updated about copy. RC1.

--- OMG Ubuntu/src/com/ohso/omgubuntu/AboutFragment.java
+++ OMG Ubuntu/src/com/ohso/omgubuntu/AboutFragment.java
@@ -1,9 +1,12 @@
 package com.ohso.omgubuntu;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
@@ -14,6 +17,12 @@ public class AboutFragment extends SherlockFragment {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         ((BaseActivity) getSherlockActivity()).getDefaultActionBar();
         getSherlockActivity().getSupportActionBar().setTitle("About OMG! Ubuntu!");
+        TextView omgCopy = (TextView) view.findViewById(R.id.activity_about_omg_copy);
+        omgCopy.setText(Html.fromHtml(getString(R.string.activity_about_omg_ubuntu)));
+        omgCopy.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView ohsoCopy = (TextView) view.findViewById(R.id.activity_about_ohso_copy);
+        ohsoCopy.setText(Html.fromHtml(getString(R.string.activity_about_ohso)));
+        ohsoCopy.setMovementMethod(LinkMovementMethod.getInstance());
         return view;
     }
 }
